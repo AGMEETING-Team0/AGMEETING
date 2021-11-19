fcbe0c9799c4e1e6c2344d2d17aa2425132a308d
adamf0x <adamfox96@gmail.com>
2021-11-18 21:53:32 -0800

    fixed component imports

--- client/App.js
+++ client/App.js
@@ -6,56 +6,53 @@ import Routes from "./routes";
 // import routes from './routes'
 import { useRoutes } from "react-router";
 
-
 //Material UI
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import { createCustomTheme } from '../config/theme';
+import { createCustomTheme } from "../config/theme";
 import { Toaster } from "react-hot-toast";
 
-import RTL from './RTL'
-import SettingsDrawer from './SettingsDrawer'
-import { THEMES } from '../config/theme/constants';
+import RTL from "./RTL";
+import SettingsDrawer from "./SettingsDrawer";
+import { THEMES } from "../config/theme/constants";
 import ErrorBoundary from "./Errorbound";
 import useSettings from "./hooks/useSettings";
 import DashboardLayout from "./Component/DashBoard/Layout";
-import Agenda from "./Component/Test2";
 
 function App(props) {
+  const { settings } = useSettings();
 
-    const { settings } = useSettings();
+  // const theme = createCustomTheme({
+  //     theme: THEMES.LIGHT
+  // });
 
-    // const theme = createCustomTheme({
-    //     theme: THEMES.LIGHT
-    // });
+  const theme = createCustomTheme({
+    direction: settings.direction,
+    responsiveFontSizes: settings.responsiveFontSizes,
+    roundedCorners: settings.roundedCorners,
+    theme: settings.theme,
+  });
 
-    const theme = createCustomTheme({
-        direction: settings.direction,
-        responsiveFontSizes: settings.responsiveFontSizes,
-        roundedCorners: settings.roundedCorners,
-        theme: settings.theme
-    });
+  const content = useRoutes(Routes);
 
-    const content = useRoutes(Routes);
+  useEffect(() => {
+    console.log("app.js");
+  });
 
-    useEffect(() => {
-        console.log('app.js')
-    })
-
-    return (
-        <ErrorBoundary>
-            <ThemeProvider theme={theme}>
-                <RTL direction={settings.direction}>
-                    {/* <CssBaseline /> */}
-                    {/* <Toaster position="top-center" /> */}
-                    {/* <SettingsDrawer /> */}
-                    {content}
-                    {/* Declarative route */}
-                    {/* <Routes /> */}
-                </RTL>
-            </ThemeProvider>
-        </ErrorBoundary>
-    );
-};
+  return (
+    <ErrorBoundary>
+      <ThemeProvider theme={theme}>
+        <RTL direction={settings.direction}>
+          {/* <CssBaseline /> */}
+          {/* <Toaster position="top-center" /> */}
+          {/* <SettingsDrawer /> */}
+          {content}
+          {/* Declarative route */}
+          {/* <Routes /> */}
+        </RTL>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
 
 export default App;
